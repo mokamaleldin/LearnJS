b9b3019eba514219308ad978e33ab3c776ef7cfd
mokamalelidn <mhmdalknj496@gmail.com>
2024-07-09 11:21:17 +0300

    feat: Refactor main.js for improved code structure and readability

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-function parent() {
-    let a = 10;
+/*
+  Function Arrow Challenges
+*/
 
-    function child() {
-        console.log(`from child ${a}`);
-        function grand() {
-            console.log(`from grand ${a}`)
-        }
-        grand();
-    }
-    child()
-}
-parent() //10 //10
+// [1] One Statement In Function
+// [2] Convert To Arrow Function
+// [3] Print The Output [Arguments May Change]
+
+let names = (...names) => {
+    // Parameter ?
+    return `string [${names.join('], [')}] => Done !`;
+};
+
+console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
+// String [Osama], [Mohamed], [Ali], [Ibrahim] => Done !
+
+/* ================================= */
+
+// [1] Replace ??? In Return Statement To Get The Output
+// [2] Create The Same Function With Regular Syntax
+// [3] Use Array Inside The Arguments To Get The Output
+
+let myNumbers = [20, 50, 10, 60];
+
+let calc = (one, two, ...nums) => one + two + nums[0];
+
+console.log(calc(10, myNumbers[3], myNumbers[2])); // 80
