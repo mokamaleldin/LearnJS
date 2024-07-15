e3d147a375e6e0819ef4f9028fe7163f5fecbe35
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-07-15 18:35:44 +0300

    chore: Remove unused code and update color scheme

--- appChangeColor/main.js
+++ appChangeColor/main.js
@@ -1,2 +1,8 @@
 
-console.log('hi')
+let lis = document.querySelectorAll("ul li");
+
+lis.forEach((li) => {
+    li.addEventListener("click", (e) => {
+        console.log(e.currentTarget.dataaset.color)
+    })
+})
